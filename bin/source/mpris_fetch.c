e04d22cd37b2bc93051880d8245873e6c5810953
LurkAndLoiter <93033540+LurkAndLoiter@users.noreply.github.com>
2025-05-14 04:01:50 -0400

    Added license header.

--- bin/source/mpris_fetch.c
+++ bin/source/mpris_fetch.c
@@ -1,3 +1,21 @@
+/* Copyright © 2025, LurkAndLoiter and contributors.
+ *
+ * This file is part of LurkAndLoiter's eww config.
+ *
+ * LurkAndLoiter's eww is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ *
+ * This is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with playerctl If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <playerctl/playerctl.h>
 #include <pulse/pulseaudio.h>
 #include <pulse/glib-mainloop.h> // Changed to use pa_glib_mainloop
